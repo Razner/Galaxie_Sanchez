52d7a7270b1fe780fcb9a448052b1622ce322a95
tilio bourigault <tilio.bourigault@outlook.com>
2023-04-05 12:09:34 +0200

    add example of mini game with gravity and keys

---
+++ static/static/js/app.js
@@ -0,0 +1,54 @@
+const config = {
+    with: 500,
+    height: 800,
+    type: Phaser.AUTO,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 200}
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+
+}
+
+
+var game = new Phaser.Game(config)
+let dude 
+let cursors
+
+function preload() {
+    this.load.image('dude', 'dude.png' )
+
+}
+
+function create() {
+    dude = this.physics.add.image(100, 100, 'dude')
+    dude.body.collideWorldBounds = true
+
+    cursors = this.input.keyboard.createCursorKeys()
+
+}
+
+function update() {
+    dude.setVelocityX(0)
+
+    if(cursors.up.isDown){
+        dude.setVelocity(0, -300)
+    }
+
+    if(cursors.right.isDown){
+        dude.setVelocity(100, 0)
+    }
+    
+    if(cursors.left.isDown){
+        dude.setVelocity(-100, 0)
+    }
+    
+    
+
+}
