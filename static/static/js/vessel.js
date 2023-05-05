63b1df6845d1488bd20c2e21ab8c3309e6ed48ee
Simon <simon.louis638@gmail.com>
2023-05-05 16:56:10 +0200

    update position vessel

--- static/static/js/vessel.js
+++ static/static/js/vessel.js
@@ -20,6 +20,26 @@ const vesselImg = document.getElementById("vessel-img");
 const vesselPrevBtn = document.getElementById("vessel-prev-btn");
 const vesselNextBtn = document.getElementById("vessel-next-btn");
 
+// Fonction pour démarrer le jeu
+function startGame() {
+  const canvas = document.getElementById("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.position = "absolute";
+  const vessel = vessels[currentVesselIndex];
+  const vesselX = (canvas.width - vessel.size) / 2;
+  const vesselY = canvas.height - vessel.size;
+  
+  // Dessiner le vaisseau aux coordonnées x et y
+  const ctx = canvas.getContext("2d");
+  const vesselImage = new Image();
+  vesselImage.src = vessel.imageSrc;
+  vesselImage.onload = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacer le contenu du canvas
+    ctx.drawImage(vesselImage, vesselX, vesselY, vessel.size, vessel.size);
+  };
+}
+
 // Fonction pour changer de vaisseau
 function updateVessel() {
   const currentVessel = vessels[currentVesselIndex];
@@ -27,19 +47,8 @@ function updateVessel() {
   vesselImg.style.width = currentVessel.size + "px"; // définir la taille de l'image
   document.getElementById("srcimg").innerHTML = currentVessel.name;
   document.getElementById("namevessel").innerHTML = currentVessel.imageSrc;
- // Dessiner le vaisseau dans le canvas
-   const canvas = document.getElementById("canvas");
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeigh;
-   canvas.style.position = "absolute";
-   const ctx = canvas.getContext("2d");
-   const vesselImage = new Image();
-   vesselImage.src = currentVessel.imageSrc;
-   vesselImage.onload = () => {
-   ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacer le contenu du canvas
-   ctx.drawImage(vesselImage, 1, 1, currentVessel.size, currentVessel.size);
- };
 
+  startGame();
 }
 
 // Gestionnaires d'événements pour les flèches de sélection de vaisseau
